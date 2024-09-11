8712878b5bcaf0ea191ebc123babfe408c5113a1
Laura Contreras <lauracontreras@MacBook-Pro-de-Laura.local>
2024-09-11 20:02:30 +0200

    creadas las clases model y repository

---
+++ src/main/java/como/laura/springboot/di/app/springboot_di/repositories/ProductRepository.java
@@ -0,0 +1,25 @@
+package como.laura.springboot.di.app.springboot_di.repositories;
+
+import java.util.Arrays;
+import java.util.List;
+
+import como.laura.springboot.di.app.springboot_di.models.Product;
+
+public class ProductRepository {
+
+    private List<Product> data;
+
+    public ProductRepository() {
+        this.data = Arrays.asList(
+            new Product(1L, "Memoria corsair 32", 300L),
+            new Product(2L, "Cpu Intel Core i9", 850L),
+            new Product(3L, "Teclado Razer Mini 60%", 85L),
+            new Product(4L, "Motherboard Gigabyte", 490L)
+        );
+    }
+
+    public List<Product> findAll(){
+        return data;
+    }
+
+}
